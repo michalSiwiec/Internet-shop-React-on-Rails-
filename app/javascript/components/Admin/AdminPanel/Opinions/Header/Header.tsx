a1675eeabd5118d7991ca4e57e01907c397c09f4
Michał <siwiec.michal724@gmail.com>
2021-04-06 20:31:07 +0200

    better styles in admin panel

--- app/javascript/components/Admin/AdminPanel/Opinions/Header/Header.tsx
+++ app/javascript/components/Admin/AdminPanel/Opinions/Header/Header.tsx
@@ -4,12 +4,13 @@ import {Link} from 'react-router-dom'
 
 const Header = () => {
     return(
-        <div className="header-container">
+        <>
             <Link to="/admin">
                 <button>Cofinj</button>
             </Link>
+
             <h2>Opinie o naszym sklepie</h2>
-        </div>
+        </>
     )
 }
 
